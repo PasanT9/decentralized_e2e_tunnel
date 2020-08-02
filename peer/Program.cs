8c6abc0efb569ec9b6691903778ac5d8f4395c7d
PasanT9 <pasan96tennakoon@gmail.com>
2020-08-02 20:46:15 +0530

    Encrypted communication between peers

--- peer/Program.cs
+++ peer/Program.cs
@@ -7,14 +7,13 @@ using System.Threading;
 using System.Collections.Generic;  
 using System.Net.Security;
 using System.Security.Authentication;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.IO;
 #if !NETSTANDARD2_0
 using System.Buffers;
 #endif
 using System.Runtime.InteropServices;
-using ProxyClient;
-using PairStream;
 using dtls_server;
 
 
@@ -37,10 +36,15 @@ namespace peer
         static int local_port;
         static IPEndPoint ipEndPoint;
         static UdpClient peer;
-
+        static Aes myAes;
         
         static void Main(string[] args)
         {
+            myAes = Aes.Create();
+            //myAes.Key = key;
+            //myAes.IV = iv;
+        myAes.Key = new byte[16] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+        myAes.IV = new byte[16] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
             string server_ip = "127.0.0.1";     // Server IP
             int server_port = 27005;   // Server port
 
@@ -102,6 +106,91 @@ namespace peer
             Console.WriteLine("Recieved address: "+connection_address);
         }
 
+        static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+                aesAlg.Padding = PaddingMode.Zeros;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+
+        static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+                aesAlg.Padding = PaddingMode.Zeros;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+
         static void req_connection(SslStream sslStream, TcpClient client)
         {
             send_message_tcp(sslStream, "REQUEST");
@@ -126,12 +215,37 @@ namespace peer
 								dtls.Unbuffer_Args="-i0 -o0";
 						}
 						dtls.Start();
-						statpair IOStream = new statpair(new StreamReader(Console.OpenStandardInput()), new StreamWriter(Console.OpenStandardOutput()));
-						new Thread(() => dtls.GetStream().CopyTo(IOStream, 16)).Start();
+                        //Stream stream = new MemoryStream();
+                        //using var sr = new StreamReader(stream);
+                        //using var sw = new StreamWriter(stream);
+						//statpair IOStream = new statpair(sr, sw);
+						//new Thread(() => dtls.GetStream().CopyTo(IOStream, 16)).Start();
+                        dtls.GetStream().Write(Encoding.Default.GetBytes("SUCCESS"));
+                        byte[] bytes;
+                        string message = "";
+                        while(String.Compare(message, "SUCCESS") != 0)
+                        {
+                            bytes = new byte[128];
+                            dtls.GetStream().Read(bytes, 0, bytes.Length);
+                            message = Encoding.UTF8.GetString(bytes);
+                            Console.Write(message);
+                        }
+
+                        Console.WriteLine();
 						while(true)
 						{
 							string input = Console.ReadLine();
+                            //byte[] encryptedData = EncryptStringToBytes_Aes((input+Environment.NewLine), myAes.Key, myAes.IV);
 							dtls.GetStream().Write(Encoding.Default.GetBytes(input+Environment.NewLine));
+                            //dtls.GetStream().Write(encryptedData);
+                            bytes = new byte[16];
+                            dtls.GetStream().Read(bytes, 0, bytes.Length);
+                            //message = Encoding.UTF8.GetString(bytes);
+                            Console.WriteLine(BitConverter.ToString(bytes).Replace("-",""));
+                            string decryptedData = DecryptStringFromBytes_Aes(bytes, myAes.Key, myAes.IV);
+                            //message = Encoding.UTF8.GetString(bytes);
+                            //Console.WriteLine("-> " + decryptedData);
+                            Console.WriteLine("-> " + decryptedData);
 						}
 						dtls.WaitForExit();
 
@@ -169,7 +283,6 @@ namespace peer
                     send_message_tcp(sslStream, "ACCEPT");
                     sslStream.Close();
                     client.Close();
-
                     DTLSServer dtls = new DTLSServer(local_port.ToString(), new byte[] {0xBA,0xA0});
 						if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)){
 								dtls.Unbuffer="winpty.exe";
@@ -180,12 +293,37 @@ namespace peer
 								dtls.Unbuffer_Args="-i0 -o0";
 						}
 						dtls.Start();
-						statpair IOStream = new statpair(new StreamReader(Console.OpenStandardInput()), new StreamWriter(Console.OpenStandardOutput()));
-						new Thread(() => dtls.GetStream().CopyTo(IOStream, 16)).Start();
+                        dtls.GetStream().Write(Encoding.Default.GetBytes("SUCCESS"));
+                        //Stream stream = new MemoryStream();
+                        //using var sr = new StreamReader(stream);
+                        //using var sw = new StreamWriter(stream);
+						//statpair IOStream = new statpair(sr, sw);
+						//new Thread(() => dtls.GetStream().CopyTo(IOStream, 16)).Start();
+                        byte[] bytes;
+                        string message = "";
+                        while(String.Compare(message, "SUCCESS") != 0)
+                        {
+                            bytes = new byte[128];
+                            dtls.GetStream().Read(bytes, 0, bytes.Length);
+                            message = Encoding.UTF8.GetString(bytes);
+                            Console.Write(message);
+                        }
+                        Console.WriteLine();
+                        
 						while(true)
 						{
+                            bytes = new byte[128];
+                            dtls.GetStream().Read(bytes, 0, bytes.Length);
+                            //string decryptedData = DecryptStringFromBytes_Aes(bytes, myAes.Key, myAes.IV);
+                            message = Encoding.UTF8.GetString(bytes);
+                            Console.WriteLine("-> " + message);
 							input = Console.ReadLine();
-							dtls.GetStream().Write(Encoding.Default.GetBytes(input+Environment.NewLine));
+                            byte[] encryptedData = EncryptStringToBytes_Aes(input, myAes.Key, myAes.IV);
+                            Console.WriteLine(BitConverter.ToString(encryptedData).Replace("-",""));
+                            //string roundtrip = DecryptStringFromBytes_Aes(encryptedData, myAes.Key, myAes.IV);
+
+							//dtls.GetStream().Write(Encoding.Default.GetBytes(input+Environment.NewLine));
+                            dtls.GetStream().Write(encryptedData);
 						}
 						dtls.WaitForExit();
 
