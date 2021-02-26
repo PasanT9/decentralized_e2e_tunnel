f318df6ff0b5c34f824a1f2b10f746f0e9957e03
Supipi96 <76469306+Supipi96@users.noreply.github.com>
2021-02-26 09:10:48 +0530

    Update Program.cs

--- test/prover/Program.cs
+++ test/prover/Program.cs
@@ -154,7 +154,7 @@ namespace prover
 
             //Time start
 	    DateTime now = DateTime.Now;
-	    Console.WriteLine("Strat Second: {0}", now.Millisecond;
+	    Console.WriteLine("Strat Second: {0}", now.Millisecond);
 
             string X = Pp.Exponent + "|";
             for (int i = 0; i < n; ++i)
